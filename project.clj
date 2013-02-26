b597d462cc8a9e08b883b78d404739cc7f99b5fe
Juergen Hoetzel <juergen@archlinux.org>
2013-02-26 12:51:38 +0100

    missing project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-nio2 "0.1.0-SNAPSHOT"
+  :description "Clojure NIO2 API"
+  :url "https://github.com/juergenhoetzel/io7"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
